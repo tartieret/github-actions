4a2702e515cf480a01691bb6209f4b6b71412c30
Thomas Tartiere <thomas.tartiere@panevo.com>
2021-04-23 14:06:29 -0700

    feat: Subtract function

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-from app.main import add_func
+from app.main import add_func, substract
 
 def test_answer():
     assert add_func(10, 5) == 15
+
+def test_substract():
+    assert substract(10, 5) == 5
